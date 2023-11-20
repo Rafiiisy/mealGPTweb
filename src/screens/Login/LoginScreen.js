0284588fb149054533b9b1e6103d9d375deb2d37
naufarrazqi <naufal.farrazqi@gmail.com>
2023-11-20 23:06:03 +0700

    Changes to be committed: 	new file:   src/assets/images/Foot LoginSignup.png 	new file:   src/assets/images/Header LoginSignup.png 	modified:   src/screens/Login/LoginScreen.js

--- src/screens/Login/LoginScreen.js
+++ src/screens/Login/LoginScreen.js
@@ -1,45 +1,54 @@
 // LoginScreen.js
 import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, TextInput, ImageBackground, TouchableOpacity, StyleSheet } from 'react-native';
+// import { createStackNavigator } from '@react-navigation/stack';
+import backgroundImage from '../../assets/images/Header LoginSignup.png'; // Adjust the path accordingly
+import Header from "../../components/common/Header";
+import Footer from "../../components/common/Footer";
 
 const LoginScreen = () => {
     const [emailPhone, setEmailPhone] = useState('');
     const [password, setPassword] = useState('');
-
     const handleLogin = () => {
         // Implement your login logic here
         console.log('Login pressed');
     };
+    const handleMenuPress = () => {
+        console.log("Menu button pressed"); // Replace with actual logic to open sidebar
+    };
 
     return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Login</Text>
+        <>
+            <Header onMenuPress={handleMenuPress} />
+            <View style={styles.container}>
+                <Text style={styles.title}>Login</Text>
+                <View style={styles.inputContainer}>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Email/phone"
+                        value={emailPhone}
+                        onChangeText={(text) => setEmailPhone(text)}
+                    />
+                </View>
 
-            <View style={styles.inputContainer}>
-                <TextInput
-                    style={styles.input}
-                    placeholder="Email or Phone"
-                    value={emailPhone}
-                    onChangeText={(text) => setEmailPhone(text)}
-                />
-            </View>
+                <View style={styles.inputContainer}>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Password"
+                        secureTextEntry
+                        value={password}
+                        onChangeText={(text) => setPassword(text)}
+                    />
+                </View>
 
-            <View style={styles.inputContainer}>
-                <TextInput
-                    style={styles.input}
-                    placeholder="Password"
-                    secureTextEntry
-                    value={password}
-                    onChangeText={(text) => setPassword(text)}
-                />
+                <View style={styles.centeredButtonContainer}>
+                    <TouchableOpacity style={styles.loginButton} onPress={handleLogin}>
+                        <Text style={styles.loginButtonText}>Login</Text>
+                    </TouchableOpacity>
+                </View>
+                <Footer />
             </View>
-
-            <View style={styles.centeredButtonContainer}>
-                <TouchableOpacity style={styles.loginButton} onPress={handleLogin}>
-                    <Text style={styles.loginButtonText}>Login</Text>
-                </TouchableOpacity>
-            </View>
-        </View>
+        </>
     );
 };
 
@@ -50,10 +59,16 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         paddingHorizontal: 20,
     },
+    backgroundImage: {
+        flex: 1,
+        resizeMode: 'contain',
+        justifyContent: 'center',
+    },
     title: {
+        font: 'mada',
         color: 'rgb(0, 85, 29)',
         fontSize: 24,
-        fontWeight: 'bold',
+        fontWeight: 'regular',
         marginBottom: 20,
     },
     inputContainer: {
@@ -68,7 +83,7 @@ const styles = StyleSheet.create({
         borderRadius: 50,
     },
     centeredButtonContainer: {
-        alignItems: 'center', // Center the child elements horizontally
+        // alignItems: 'center', // Center the child elements horizontally
     },
     loginButton: {
         backgroundColor: 'rgb(0, 153, 52)',
