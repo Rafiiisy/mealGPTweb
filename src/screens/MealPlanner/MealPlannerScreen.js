9be13e8eec64ff2701bbfde57025ae6209034b0a
Tuwmore <rifki7286@gmail.com>
2023-11-21 00:24:45 +0700

    Changes to be committed: 	modified:   src/screens/MealPlanner/MealPlannerScreen.js 	new file:   src/screens/MealPlanner/MealPlannerStyle.js

---
+++ src/screens/MealPlanner/MealPlannerScreen.js
@@ -0,0 +1,47 @@
+import React from "react";
+import {
+  StyleSheet,
+  View,
+  Image,
+  Text,
+  TextInput,
+  SafeAreaView,
+  ScrollView,
+  FlatList,
+} from "react-native";
+
+import styles from "../MealPlanner/MealPlannerStyle";
+import Header from "../../components/common/Header";
+import Footer from "../../components/common/Footer";
+const MealPlannerScreen = () => {
+    return (
+        <View>
+            <Header/>
+            <View style={styles.containerButton}>
+                <Image
+                    source={require("../../../assets/ocupied1.png")}
+                    style={styles.imageSize}
+                />
+            </View>
+
+            <View style={styles.containerButton}>
+                <Image
+                    source={require("../../../assets/ocupied2.png")}
+                    style={styles.imageSize}
+                />
+            </View>
+
+            <View style={styles.containerButton}>
+                <Image
+                    source={require("../../../assets/Empty.png")}
+                    style={styles.imageSize}
+                />
+            </View>
+            <Footer/>
+        </View>
+            
+      
+    );
+  }
+
+export default MealPlannerScreen;
